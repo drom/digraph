d2f0c108bef9f35a1c37319a5d345379f9af5391
drom <alex.drom@gmail.com>
2014-03-28 23:31:29 -0700

    some iterative fixes

--- digraph.js
+++ digraph.js
@@ -3,57 +3,58 @@
 
 var digraph = digraph || {
 	random: function () {
-		this.node_indx = {a:0, b:1, c:2};
-		this.edge_from = [{1:{}, 2:{}}, {2:{}}, {}];
-		this.edge_toto = [{}, {0:true}, {0:true,1:true}];
-		this.node_attr = [{}, {}, {}];
+		this.v.count   = 4;
+		this.v.id      = {'a':0, 'b':1, 'c':2, 'd':3};
+		this.v.name    = {0:'a', 1:'b', 2:'c', 3:'d'};
+		this.e.count   = 3;
+		this.e.vs      = {0:[0,1], 1:[0,2], 2:[1,3]};
+		this.e.o       = {0:{1:0, 2:1}, 1:{3:2}};
+		this.e.i       = {1:{0:0}, 3:{1:2}, 2:{0:1}};
 	},
 	vertices: function () {
 		var k, res = [];
-		for (k in this.node_indx) {
+		for (k in this.v.id) {
 			res.push(k);
 		}
 		return res;
 	},
 	edges: function  () {
-		var i, ilen, edges, k, res = [];
-		ilen = this.edge_from.length;
-		for (i = 0; i < ilen; i++) {
-			edges = this.edge_from[i];
-			for (k in edges) {
-				res.push([i, k]);
-			}
+		var k, ev, e = this.e, vn = this.v.name, res = [];
+		for (k in e.vs) {
+			ev = e.vs[k];
+			res.push([vn[ev[0]], vn[ev[1]]]);
 		}
 		return res;
 	},
 	in_degree: function (v) {
-		var vindex, edges, res;
-		vindex = (this.node_indx[v])|0;
-		edges  = this.edge_from [vindex];
-		res = (Object.keys(edges).length)|0;
+		var vid, edges, res;
+		vid = this.v.id [v];
+		if ('undefined' === typeof vid) { return 0; }
+		edges  = this.e.i [vid];
+		if ('undefined' === typeof edges) { return 0; }
+		res = Object.keys(edges).length;
 		return res;
 	},
 	out_degree: function (v) {
-		var vindex, edges, res;
-		vindex = (this.node_indx[v])|0;
-		edges  = this.edge_toto [vindex];
-		res = (Object.keys(edges).length)|0;
+		var vid, edges, res;
+		vid = this.v.id [v];
+		if ('undefined' === typeof vid) { return 0; }
+		edges  = this.e.o [vid];
+		if ('undefined' === typeof edges) { return 0; }
+		res = Object.keys(edges).length;
 		return res;
 	},
 	edges_at: function (v) {
-		var edges = v;
-		return edges;
+		return v;
 	},
 	get_vertex_attribute: function (v, label) {
-		var a = [v,label];
-		return a;
+		return [v,label];
 	},
 	get_edge_attribute: function (e, label) {
-		var a = [e,label];
-		return a;
+		return [e,label];
 	},
-	has_node: function (name) {
-		return this.node_indx[name] !== undefined || false;
+	has_vertex: function (name) {
+		return this.v.id[name] !== undefined || false;
 	},
 	has_edge: function (nn) {
 		var a, b;
@@ -65,7 +66,7 @@ var digraph = digraph || {
 		var i;
 		if (!this.has_node(name)) {
 			i = this.node_count++;
-			this.node_indx[name] = i;
+			this.v.id[name] = i;
 			this.node_attr[i] = {};
 		}
 		console.log('Node: ', name);
@@ -73,8 +74,8 @@ var digraph = digraph || {
 	add_edge: function (aname) {
 		var fr, to;
 		aname.forEach(this.add_node);
-		fr = this.node_indx[aname[0]];
-		to = this.node_indx[aname[1]];
+		fr = this.v.id[aname[0]];
+		to = this.v.id[aname[1]];
 		if (!this.has_edge([fr, to])) {
 			this.edge_from[fr] = this.edge_from[fr] || {};
 			this.edge_from[fr][to] = {};
@@ -122,31 +123,33 @@ var digraph = digraph || {
 		}
 		return this.single_add(obj);
 	},
-	write: function () {
-		var vs, i, ilen, ret = [];
+	write_dot: function () {
+		var k, v = this.v, e = this.e, es, ret = 'digraph g {\n';
 
-		vs = this.vertices ();
-		ilen = vs.length;
-		for (i = 0; i < ilen; i++) {
-			ret.push ([vs[i]]);
+		for (k in v.id) {
+			ret += '  ' + k + ';\n';
 		}
+		for (k in e.vs) {
+			es = e.vs[k];
+			ret += '  ' + v.name[es[0]] + ' -> ' + v.name[es[1]] + ';\n';
+		}
+		ret += '}';
 		return ret;
 	}
 };
 
 module.exports = function () {
 	return {
-		node_count: 0,
-		node_indx:  {},
-		edge_from:  [],
-		edge_toto:  [],
-		node_attr:  [],
+		v: {count:0, id:{}, name:{}},
+		e: {count:0, vs:{}, o:{}, i:{}},
 		random:     digraph.random,
+		has_vertex: digraph.has_vertex,
+		has_edge:   digraph.has_edge,
 		vertices:   digraph.vertices,
 		edges:      digraph.edges,
 		in_degree:  digraph.in_degree,
 		out_degree: digraph.out_degree,
 		add:        digraph.add,
-		write:      digraph.write
+		write_dot:  digraph.write_dot
 	};
 };
--- t1.js
+++ t1.js
@@ -1,13 +1,36 @@
 var digraph = require('./digraph');
 
 g1 = digraph();
-//g2 = digraph();
-
 g1.random();
 
-console.log(g1.write());
-//console.log(g1.edges());
-//console.log(g2.vertices());
+console.log(g1.e);
+
+console.log(g1.v);
+
+console.log(g1.vertices());
+
+console.log(g1.edges());
+
+console.log(g1.write_dot());
+
+(function(g){
+	var i, ilen, vs = g.vertices();
+	for (i = 0, ilen = vs.length; i < ilen; i++) {
+		console.log(g.in_degree(vs[i]));	
+	}
+})(g1);
+
+(function(g){
+	var i, ilen, vs = g.vertices();
+	for (i = 0, ilen = vs.length; i < ilen; i++) {
+		console.log(g.out_degree(vs[i]));	
+	}
+})(g1);
+
+//g2 = digraph();
+
+//g1.random();
+
 //console.log(g1.in_degree('b'));
 //console.log(g1.out_degree('a'));
 //console.log(g1);
