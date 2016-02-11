4c58e279770bfa815c168d8e7a46b06d89fbb79b
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-02-10 23:01:27 -0800

    added export for dot

--- test/basic.js
+++ test/basic.js
@@ -10,7 +10,7 @@ describe ('digraph', function () {
         g1 = digraph();
         a = g1.add.node();
         b = g1.add.node();
-        c = g1.add.node();
+        c = g1.add.node(); c.id = 'c';
         d = g1.add.node();
         g1.add.edge(a, b);
         g1.add.edge(b, c);
@@ -26,13 +26,6 @@ describe ('digraph', function () {
         expect(g1.get.nodes().length).to.equal(4);
     });
 
-    it ('dump digraph', function () {
-        expect(g1.dump()).to.deep.equal({
-            edges: [[0, 1, {}], [1, 2, {}], [2, 0, {}], [1, 3, {}]],
-            nodes: [{}, {}, {}, {}]
-        });
-    });
-
     it ('in degree 1', function () {
         expect(g1.get.in.edges(a).length).to.equal(1);
     });
@@ -43,6 +36,17 @@ describe ('digraph', function () {
 
     it ('out degree 2', function () {
         expect(g1.get.out.edges(b).length).to.equal(2);
+    });
+
+    it ('dump digraph', function () {
+        expect(g1.export.obj()).to.deep.equal({
+            edges: [[0, 1, {}], [1, 2, {}], [2, 0, {}], [1, 3, {}]],
+            nodes: [{}, {}, {id:'c'}, {}]
+        });
+    });
+
+    it ('export dot', function () {
+        expect(g1.export.dot()).to.equal('digraph {\n  _0 -> _1\n  _1 -> c\n  c -> _0\n  _1 -> _3\n}\n');
     });
 
 });
